ff0c27bbfaeb42ff5961b579b77264eda28efcf5
LongC2200006 <longc2200006@student.ctu.edu.vn>
2024-01-06 20:54:28 +0700

    design autoload, footer, header

--- layouts/header.php
+++ layouts/header.php
@@ -1,7 +1,4 @@
-<?php 
-    require_once ("autoload/autoload.php");
-    
-?>
+
 <!DOCTYPE html>
 <html lang="en">
    <head>
@@ -11,18 +8,18 @@
       <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="public/css/open-iconic-bootstrap.min.css">
-      <link rel="stylesheet" href="public/css/animate.css">
-      <link rel="stylesheet" href="public/css/owl.carousel.min.css">
-      <link rel="stylesheet" href="public/css/owl.theme.default.min.css">
-      <link rel="stylesheet" href="public/css/magnific-popup.css">
-      <link rel="stylesheet" href="public/css/aos.css">
-      <link rel="stylesheet" href="public/css/ionicons.min.css">
-      <link rel="stylesheet" href="public/css/bootstrap-datepicker.css">
-      <link rel="stylesheet" href="public/css/jquery.timepicker.css">
-      <link rel="stylesheet" href="public/css/flaticon.css">
-      <link rel="stylesheet" href="public/css/icomoon.css">
-      <link rel="stylesheet" href="public/css/style.css">
+      <link rel="stylesheet" href="/blogs/public/css/open-iconic-bootstrap.min.css">
+      <link rel="stylesheet" href="/blogs/public/css/animate.css">
+      <link rel="stylesheet" href="/blogs/public/css/owl.carousel.min.css">
+      <link rel="stylesheet" href="/blogs/public/css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="/blogs/public/css/magnific-popup.css">
+      <link rel="stylesheet" href="/blogs/public/css/aos.css">
+      <link rel="stylesheet" href="/blogs/public/css/ionicons.min.css">
+      <link rel="stylesheet" href="/blogs/public/css/bootstrap-datepicker.css">
+      <link rel="stylesheet" href="/blogs/public/css/jquery.timepicker.css">
+      <link rel="stylesheet" href="/blogs/public/css/flaticon.css">
+      <link rel="stylesheet" href="/blogs/public/css/icomoon.css">
+      <link rel="stylesheet" href="/blogs/public/css/style.css">
    </head>
    <body>
       <div id="colorlib-page">
@@ -30,7 +27,9 @@
          <aside id="colorlib-aside" role="complementary" class="js-fullheight">
             <nav id="colorlib-main-menu" role="navigation">
                <ul>
+                  <li><img src="/blogs/img/Screenshot 2024-01-06 193822.jpg" class="rounded-circle shadow-4"style="width: 100px;" alt="Avatar" />  Welcome back</li>
                   <li class="colorlib-active"><a href="index.html">Home</a></li>
+                  <li><a href="/blogs/modules/category">category</a></li>
                   <li><a href="fashion.html">Fashion</a></li>
                   <li><a href="travel.html">Travel</a></li>
                   <li><a href="about.html">About</a></li>
@@ -39,15 +38,7 @@
             </nav>
             <div class="colorlib-footer">
                <h1 id="colorlib-logo" class="mb-4"><a href="index.html" style="background-image: url(img/bg_1.jpg);">Andrea <span>Moore</span></a></h1>
-               <div class="mb-4">
-                  <h3>Subscribe for newsletter</h3>
-                  <form action="#" class="colorlib-subscribe-form">
-                     <div class="form-group d-flex">
-                        <div class="icon"><span class="icon-paper-plane"></span></div>
-                        <input type="text" class="form-control" placeholder="Enter Email Address">
-                     </div>
-                  </form>
-               </div>
+               
                <p class="pfooter">
                   <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                   Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
