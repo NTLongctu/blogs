c38bcc0523bd28c1f2c4b637457c9fe06383b257
LongC2200006 <longc2200006@student.ctu.edu.vn>
2024-01-10 21:31:43 +0700

    fix bugs comment index myblog page

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
    require_once ("autoload/autoload.php");
    //$sql ="SELECT *,category.name AS catename FROM blog,category WHERE category.id = blog.tag_id AND user_id =".$_SESSION['name_id']." ORDER BY ID DESC";
-   $sql = "SELECT blog.*,category.name AS catename FROM blog LEFT JOIN category ON category.id=blog.tag_id ORDER BY ID DESC";
+   $sql = "SELECT blog.id AS blogid, 
+                  blog.title AS blogtitle, 
+                  blog.subdescription AS blogsub, 
+                  blog.date_create AS blogdate_create, 
+                  blog.image AS blogimg, 
+                  category.name AS catename, 
+                  COUNT(comments.id) AS CommentCount 
+         FROM blog LEFT JOIN comments ON blog.id = comments.blog_id 
+         LEFT JOIN category ON category.id = blog.tag_id 
+         GROUP BY blogid, blogtitle, blogsub, blogdate_create, blogimg, catename ORDER BY blog.id DESC";
    $myblog =$db->fetchsql($sql);
+   $category = $db->fetchAll('category');
+
 
 ?>
 <?php require_once("layouts/header.php"); ?>
@@ -11,19 +22,19 @@
                            <?php $stt = 1; foreach ($myblog as $item) : ?>
                               <div class="col-md-12">
                                  <div class="blog-entry ftco-animate d-md-flex">
-                                    <a href="single.html" class="img img-2" style="background-image: url(img/<?php echo $item['image'];?>);"></a>
+                                    <a href="single.html" class="img img-2" style="background-image: url(img/<?php echo $item['blogimg'];?>);"></a>
                                     <div class="text text-2 pl-md-4">
-                                       <h3 class="mb-2"><a href="viewdetail.php?id=<?php echo$item['id'];?>"><?php echo $item['title']; ?></a></h3>
+                                       <h3 class="mb-2"><a href="viewdetail.php?id=<?php echo$item['blogid'];?>"><?php echo $item['blogtitle']; ?></a></h3>
                                        <div class="meta-wrap">
                                           <p class="meta">
-                                             <span><i class="icon-calendar mr-2"></i><?php echo convertDate($item['date_create']);?></span>
+                                             <span><i class="icon-calendar mr-2"></i><?php echo convertDate($item['blogdate_create']);?></span>
                                              <span><a href="single.html"><i class="icon-folder-o mr-2"></i></a>
                                              <?php echo $item['catename']; ?></span>
-                                             <span><i class="icon-comment2 mr-2"></i><?php echo $item['id'];?> Comment</span>
+                                             <span><i class="icon-comment2 mr-2"></i><?php echo $item['CommentCount']; ?> Comment</span>
                                           </p>
                                        </div>
-                                       <p class="mb-4"><?php echo $item['subdescription']; ?></p>
-                                       <p><a href="editmyblog.php?id=<?php echo$item['id'];?>" class="btn-custom">Edit your blog <span class="ion-ios-arrow-forward"></span></a></p>
+                                       <p class="mb-4"><?php echo $item['blogsub']; ?></p>
+                                       <p><a href="viewdetail.php?id=<?php echo$item['blogid'];?>" class="btn-custom">View detail blog <span class="ion-ios-arrow-forward"></span></a></p>
                                     </div>
                                  </div>
                               </div>
